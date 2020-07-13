02d2f028eea8204fcbd4640c20b8f1feca30703b
Valerie <valerie.flores64@gmail.com>
2020-07-12 22:24:02 -0700

    updated port for Heroku deployment

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser');
 const app = express();
-const port = 3030;
+const port = process.env.PORT || 3030;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
